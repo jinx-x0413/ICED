f81839cf5339b7420d327e68fd8e63fe022a99cb
BXJin <bsung472@gmail.com>
2025-03-18 11:06:29 +0900

    webserver 파일구조 .gitkeep은 삭제하고 만드셈

---
+++ web/WebServer/ICED_backend/src/main/java/com/iced_backend/IcedBackendApplication.java
@@ -0,0 +1,13 @@
+package all.iced_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcedBackendApplication.class, args);
+	}
+
+}
---
+++ web/WebServer/ICED_backend/src/test/java/all/iced_backend/IcedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package all.iced_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
