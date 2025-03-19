e9a070eaa4aee9884282075fda6d9c8ffe63d127
Jinx <khksda3@gmail.com>
2025-03-19 21:04:35 +0900

    ▪️ [Core] CorePlayerController --|> ProfilingPlayerController

--- UEClient/UEViewer/Source/UEViewer/Core/CorePlayerController.h
+++ UEClient/UEViewer/Source/UEViewer/Core/CorePlayerController.h
@@ -4,11 +4,16 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+
+#include "Service/Utility/Profiling/Public/ProfilingPlayerController.h"
+
 #include "CorePlayerController.generated.h"
 
 
+
+
 UCLASS()
-class UEVIEWER_API ACorePlayerController : public APlayerController
+class UEVIEWER_API ACorePlayerController : public AProfilingPlayerController
 {
 	GENERATED_BODY()
 
