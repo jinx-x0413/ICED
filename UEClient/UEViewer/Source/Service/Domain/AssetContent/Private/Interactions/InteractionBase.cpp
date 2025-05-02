9c7b50331d9481e5a98c03f7f98ecca7cb229898
Jinx <khksda3@gmail.com>
2025-05-02 20:03:14 +0900

    ▪️ [AssetContnet] [InteractionBase] GetTargetActorCenter 수정

--- UEClient/UEViewer/Source/Service/Domain/AssetContent/Private/Interactions/InteractionBase.cpp
+++ UEClient/UEViewer/Source/Service/Domain/AssetContent/Private/Interactions/InteractionBase.cpp
@@ -31,10 +31,11 @@ FVector UInteractionBase::GetTargetActorCenter()
 {
 	if (!IsValid(TargetData.TargetActor))
 	{
-		UE_LOG(LogTemp, Warning, TEXT("TargetActor is Invalid at UTransformInteraction"));
-		return FVector::ZeroVector;
+		UE_LOG(LogTemp, Warning, TEXT("TargetActor is Invalid at InteractionBase"));
+		return FVector();
 	}
 
+	// 액터의 모든 컴포넌트를 고려한 최소, 최대 바운드 계산
 	FVector ActorBoundsMin(FLT_MAX, FLT_MAX, FLT_MAX);
 	FVector ActorBoundsMax(-FLT_MAX, -FLT_MAX, -FLT_MAX);
 
@@ -42,15 +43,19 @@ FVector UInteractionBase::GetTargetActorCenter()
 	{
 		if (UPrimitiveComponent* PrimitiveComponent = Cast<UPrimitiveComponent>(Component))
 		{
-			FBoxSphereBounds Bounds = PrimitiveComponent->Bounds; // 월드 기준 바운드
-			FVector WorldBoundsMin = Bounds.Origin - Bounds.BoxExtent;
-			FVector WorldBoundsMax = Bounds.Origin + Bounds.BoxExtent;
+			FVector ComponentBoundsMin, ComponentBoundsMax;
+			//PrimitiveComponent->GetCollisionBounds(true, ComponentBoundsMin, ComponentBoundsMax);
+			// 월드 좌표계로 변환
+			FVector WorldBoundsMin = PrimitiveComponent->GetComponentTransform().TransformPosition(ComponentBoundsMin);
+			FVector WorldBoundsMax = PrimitiveComponent->GetComponentTransform().TransformPosition(ComponentBoundsMax);
 
+			// 최소, 최대 값 갱신
 			ActorBoundsMin = ActorBoundsMin.ComponentMin(WorldBoundsMin);
 			ActorBoundsMax = ActorBoundsMax.ComponentMax(WorldBoundsMax);
 		}
 	}
 
+	// 액터의 메시 바운드를 기준으로 중앙값 계산
 	return (ActorBoundsMin + ActorBoundsMax) / 2.0f;
 }
 
